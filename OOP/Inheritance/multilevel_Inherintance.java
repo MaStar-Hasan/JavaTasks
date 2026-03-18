7fc4c10b1b4b476244d3e1d728fbc0701f38fc86
MaStar-Hasan <hasanmohemmed123@gmail.com>
2026-03-18 10:46:20 +0530

    Implement multilevel inheritance example with vehicle, fourWheeler, and car classes

---
+++ OOP/Inheritance/multilevel_Inherintance.java
@@ -0,0 +1,27 @@
+package OOP.Inheritance;
+
+class vehicle {
+    vehicle() {
+        System.out.println("this is vehicle ");
+    }
+}
+
+class fourWheeler extends vehicle {
+    fourWheeler() {
+        System.out.println("this is four wheeler");
+    }
+}
+
+class car extends fourWheeler {
+    car() {
+        System.out.println("this fourwheeler is car");
+    }
+}
+
+public class multilevel_Inherintance {
+    public static void main(String[] args) {
+        car c = new car();
+        fourWheeler f = new fourWheeler();
+        vehicle v = new vehicle();
+    }
+}
